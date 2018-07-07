49a77885b4d7c656cfa8ef1033cbf5b48b8191ad
new2scala <jiajiwu.icare@gmail.com>
2018-07-07 03:30:27 +0200

    adding scala dep

---
+++ dl4j-examples/src/main/scala/org/ditw/learning/dl4j/feed4wd/PH.scala
@@ -0,0 +1,8 @@
+package org.ditw.learning.dl4j.feed4wd
+
+object PH extends App {
+  println("hello")
+
+  val l = List("a", "b")
+  println(l.mkString("(", ",", ")"))
+}
---
+++ dl4j-examples/src/main/scala/org/ditw/learning/dl4j/feed4wd/WVec1.scala
@@ -0,0 +1,5 @@
+package org.ditw.learning.dl4j.feed4wd
+
+class WVec1 {
+
+}
