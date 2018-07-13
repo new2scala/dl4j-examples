26e7212d7b7da847ceb46fb8b7d12cddf9c15e58
new2scala <jiajiwu.icare@gmail.com>
2018-07-14 01:42:02 +0200

    gd_all

--- dl4j-examples/src/main/scala/org/ditw/learning/mlgen/gd/PlotDataSet.scala
+++ dl4j-examples/src/main/scala/org/ditw/learning/mlgen/gd/PlotDataSet.scala
@@ -1,6 +1,7 @@
 package org.ditw.learning.mlgen.gd
 
 import java.awt.Color
+import java.util
 import java.util.Random
 
 import javafx.scene.chart.NumberAxis
@@ -28,7 +29,8 @@ object PlotDataSet extends App {
 
   trace(genData(10))
 
-  def y(x:Double):Double = x*x + x + 3.25
+  def y(x:Double):Double =
+    x*x + x + 3.25
 
   val xs = (0 to 100).map(x => x*0.5-25)
   val ys = xs.indices.map(idx => y(xs(idx))+rand.nextGaussian()*2)
@@ -37,8 +39,78 @@ object PlotDataSet extends App {
 
   val c: XYSeriesCollection = new XYSeriesCollection
   c.addSeries(series)
-  val s2 = gd1(30.0, 0.2)
-  c.addSeries(s2)
+//  val s2 = gd1(30.0, 0.2)
+//  c.addSeries(s2)
+
+  // ( (a0*x^2 + a1*x + a2) - y )^2
+  def error(params:Array[Double], xs:Array[Double], ys:Array[Double]):Double = {
+    def v(a1:Double, a2:Double, a3:Double, x:Double):Double = a1*x*x + a2*x + a3
+    xs.indices.map { idx =>
+      val x = xs(idx)
+      val d = v(params(0), params(1), params(2), x) - ys(idx)
+      d*d
+    }.sum
+  }
+
+  // d/d_a0 ( (a0*x^2 + a1*x + a2) - y )^2 = 2*a0*x^4 + 2*x^2((a1*x+a2)-y)
+  def errorD_a0(params:Array[Double], x:Double, y:Double):Double = {
+    val (a0, a1, a2) = (params(0), params(1), params(2))
+
+    2*a0*math.pow(x, 4) + 2*x*x*(a1*x+a2-y)
+  }
+
+  // d/d_a1 ( (a0*x^2 + a1*x + a2) - y )^2 = 2*a1*x^2 + 2*x((a0*x^2+a2)-y)
+  def errorD_a1(params:Array[Double], x:Double, y:Double):Double = {
+    val (a0, a1, a2) = (params(0), params(1), params(2))
+    2*a1*x*x + 2*x*(a0*x*x+a2-y)
+  }
+
+  // d/d_a2 ( (a0*x^2 + a1*x + a2) - y )^2 = 2*a2 + 2*((a0*x^2+a1*x)-y)
+  def errorD_a2(params:Array[Double], x:Double, y:Double):Double = {
+    val (a0, a1, a2) = (params(0), params(1), params(2))
+    2*a2 + 2*(a0*x*x+a1*x-y)
+  }
+
+  def gd_all(params:Array[Double], xs:Array[Double], ys:Array[Double]):Array[Double] = {
+    val res = Array(0.0, 0.0, 0.0)
+    xs.indices.foreach { idx =>
+      res(0) += errorD_a0(params, xs(idx), ys(idx))
+      res(1) += errorD_a1(params, xs(idx), ys(idx))
+      res(2) += errorD_a2(params, xs(idx), ys(idx))
+    }
+    res.map(r => r/xs.size)
+  }
+
+  val initParams = Array(0.0, 0.0, 0.0)
+
+  def fit(
+           initParams:Array[Double],
+           xs:Array[Double],
+           ys:Array[Double],
+           learningRate:Double,
+           epochs:Int,
+           gd:(Array[Double], Array[Double], Array[Double]) => Array[Double]
+         ):Unit = {
+    var params = util.Arrays.copyOf(initParams, initParams.length)
+
+    (0 until epochs).foreach { _ =>
+
+      val err = error(params, xs, ys)
+      val gdr = gd(params, xs, ys)
+      params = params.indices.map { idx =>
+        params(idx) - learningRate * gdr(idx)
+      }.toArray
+
+      val paramTr = params.map(p => f"$p%.5f").mkString(",")
+
+      println(
+        f"$paramTr:\t $err%.5f"
+      )
+    }
+  }
+
+  fit(initParams, xs.toArray, ys.toArray, 0.00001, 10000, gd_all)
+  println("ok")
 
   def gd1(initVal:Double, learningRate:Double):XYSeries = {
     val res = new XYSeries("gd", false)
