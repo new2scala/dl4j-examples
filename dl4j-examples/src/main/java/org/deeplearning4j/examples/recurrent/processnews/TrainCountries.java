4909404cd1ab4c4162f9cbe4a92be03e7298a9bf
new2scala <jiajiwu.icare@gmail.com>
2018-08-07 22:30:48 +0200

    test directly after training

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/processnews/TrainCountries.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/processnews/TrainCountries.java
@@ -1,10 +1,12 @@
 package org.deeplearning4j.examples.recurrent.processnews;
 
+import org.apache.commons.io.IOUtils;
 import org.datavec.api.util.ClassPathResource;
 import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.examples.nlp.word2vec.W2VAffsFull;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
+import org.deeplearning4j.nn.conf.BackpropType;
 import org.deeplearning4j.nn.conf.GradientNormalization;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
@@ -18,12 +20,23 @@ import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFac
 import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
 import org.deeplearning4j.util.ModelSerializer;
 import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.indexing.INDArrayIndex;
+import org.nd4j.linalg.indexing.NDArrayIndex;
 import org.nd4j.linalg.learning.config.RmsProp;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TrainCountries {
 
@@ -39,9 +52,9 @@ public class TrainCountries {
         DATA_PATH = rootDir;
         WORD_VECTORS_PATH = "/media/sf_vmshare/aff-w2v-full.model";
 
-        int batchSize = 32;     //Number of examples in each minibatch
-        int nEpochs = 2;        //Number of epochs (full passes of training data) to train on
-        int truncateReviewsToLength = 25;  //Truncate reviews with length (# words) greater than this
+        int batchSize = 128;     //Number of examples in each minibatch
+        int nEpochs = 20;        //Number of epochs (full passes of training data) to train on
+        int truncateReviewsToLength = 20;  //Truncate reviews with length (# words) greater than this
 
         //DataSetIterators for training and testing respectively
         //Using AsyncDataSetIterator to do data loading in a separate thread; this may improve performance vs. waiting for data to load
@@ -49,7 +62,9 @@ public class TrainCountries {
         wordVectors = WordVectorSerializer.readWord2VecModel(new File(WORD_VECTORS_PATH));
         log.info("Done loading w2v");
 
-        TokenizerFactory tokenizerFactory = new DefaultTokenizerFactory();
+        loadCategoryMap("/media/sf_vmshare/aff-w2v-td/categories.txt");
+
+        tokenizerFactory = new DefaultTokenizerFactory();
         tokenizerFactory.setTokenPreProcessor(new CommonPreprocessor());
 
         CountryIterator iTrain = new CountryIterator.Builder()
@@ -61,14 +76,14 @@ public class TrainCountries {
             .train(true)
             .build();
 
-        CountryIterator iTest = new CountryIterator.Builder()
-            .dataDirectory(DATA_PATH)
-            .wordVectors(wordVectors)
-            .batchSize(batchSize)
-            .tokenizerFactory(tokenizerFactory)
-            .truncateLength(truncateReviewsToLength)
-            .train(false)
-            .build();
+//        CountryIterator iTest = new CountryIterator.Builder()
+//            .dataDirectory(DATA_PATH)
+//            .wordVectors(wordVectors)
+//            .batchSize(batchSize)
+//            .tokenizerFactory(tokenizerFactory)
+//            .truncateLength(truncateReviewsToLength)
+//            .train(false)
+//            .build();
 
         //DataSetIterator train = new AsyncDataSetIterator(iTrain,1);
         //DataSetIterator test = new AsyncDataSetIterator(iTest,1);
@@ -80,18 +95,23 @@ public class TrainCountries {
         tokenizerFactory = new DefaultTokenizerFactory();
         tokenizerFactory.setTokenPreProcessor(new CommonPreprocessor());
         //Set up network configuration
-        int lstmLayerSize = 400;
+        int lstmLayerSize = 512;
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-            .updater(new RmsProp(0.0005))
+            .updater(new RmsProp(0.0001))
             .l2(1e-5)
             .weightInit(WeightInit.XAVIER)
             .gradientNormalization(GradientNormalization.ClipElementWiseAbsoluteValue).gradientNormalizationThreshold(1.0)
             .list()
             .layer(0, new LSTM.Builder().nIn(inputNeurons).nOut(lstmLayerSize)
-                .activation(Activation.RELU).build())
+                .activation(Activation.SOFTSIGN).build())
             .layer(1, new RnnOutputLayer.Builder().activation(Activation.SOFTMAX)
                 .lossFunction(LossFunctions.LossFunction.MCXENT).nIn(lstmLayerSize).nOut(outputs).build())
-            .pretrain(false).backprop(true).build();
+            .pretrain(false)
+            .backprop(true)
+            .backpropType(BackpropType.TruncatedBPTT)
+            .tBPTTBackwardLength(6)
+            .tBPTTForwardLength(6)
+            .build();
 
         MultiLayerNetwork net = new MultiLayerNetwork(conf);
         net.init();
@@ -109,9 +129,128 @@ public class TrainCountries {
 //            System.out.println(evaluation.stats());
 
             ModelSerializer.writeModel(net, modelPath, true);
+
+            runTests(
+                net,
+                new String[]{
+                    "1 virginia commonwealth university school of nursing richmond virginia",
+                    "102nd hospital of chinese pla",
+                    "1 gene experiment center institute of applied biochemistry university of tsukuba tsukuba-city 305",
+                    "1 direktion pflege und mttb universitätsspital zürich",
+                    "11 education centre freeman hospital newcastle upon tyne",
+                    "1 best practice advocacy centre new zealand dunedin",
+                    "**division of gastroenterology and hepatology medical university of vienna vienna",
+                    "1 department of psychology princeton university"
+                },
+                new String[]{
+                    "United States",
+                    "China",
+                    "Japan",
+                    "Switzerland",
+                    "United Kingdom",
+                    "New Zealand",
+                    "Austria",
+                    "United States"
+                }
+            );
         }
 
         System.out.println("----- Example complete -----");
+    }
+
+    private static Map<Integer, String> categoryMap;
+    private static Map<String, Integer> revCategoryMap;
+    private static void loadCategoryMap(String categoryPath) throws Exception {
+        categoryMap = new HashMap<>();
+        revCategoryMap = new HashMap<>();
+
+        List<String> lines = IOUtils.readLines(new FileInputStream(categoryPath), StandardCharsets.UTF_8);
+        for (String l : lines) {
+            String[] parts = l.split(",");
+            int cat = Integer.parseInt(parts[0]);
+            String countryName = parts[1];
+            categoryMap.put(cat, countryName);
+            revCategoryMap.put(countryName, cat);
+        }
+    }
+
+    private static void runTests(MultiLayerNetwork model, String[] inputs, String[] expCountries) {
+        for (int i = 0; i < inputs.length; i ++) {
+            String input = inputs[i];
+            String expCountry = expCountries[i];
+            int expCat = revCategoryMap.get(expCountry);
+            DataSet ds = prepareTestData(input);
+            INDArray feats = ds.getFeatureMatrix();
+
+            INDArray res = model.output(feats, false);
+            int[] arrSize = res.shape();
+
+            double max = Double.MIN_VALUE;
+            int cat = -1;
+            String expCountryVal = "N/A";
+            for (int j = 0; j < arrSize[1]; j ++) {
+                Double v = (Double)res.getColumn(j).sumNumber();
+                if (max < v) {
+                    max = v;
+                    cat = j;
+                }
+
+                if (j == expCat)
+                    expCountryVal = String.format("expected [%s/%.4f]", expCountry, v);
+            }
+            String resCat = categoryMap.get(cat);
+            System.out.println(
+                String.format("%s/%.4f(%s):\t%s", resCat, max, expCountryVal, input)
+            );
+        }
+    }
+
+    private static DataSet prepareTestData(String input) {
+        List<String> news = new ArrayList<>(1);
+        int[] category = new int[1];
+        int currCategory = 0;
+        news.add(input);
+
+        List<List<String>> allTokens = new ArrayList<>(news.size());
+        int maxLength = 0;
+        for (String s : news) {
+            List<String> tokens = tokenizerFactory.create(s).getTokens();
+            List<String> tokensFiltered = new ArrayList<>();
+            for (String t : tokens) {
+                if (wordVectors.hasWord(t)) tokensFiltered.add(t);
+            }
+            allTokens.add(tokensFiltered);
+            maxLength = Math.max(maxLength, tokensFiltered.size());
+        }
+
+        INDArray features = Nd4j.create(news.size(), wordVectors.lookupTable().layerSize(), maxLength);
+        INDArray labels = Nd4j.create(news.size(), 4, maxLength);    //labels: Crime, Politics, Bollywood, Business&Development
+        INDArray featuresMask = Nd4j.zeros(news.size(), maxLength);
+        INDArray labelsMask = Nd4j.zeros(news.size(), maxLength);
+
+        int[] temp = new int[2];
+        for (int i = 0; i < news.size(); i++) {
+            List<String> tokens = allTokens.get(i);
+            temp[0] = i;
+            for (int j = 0; j < tokens.size() && j < maxLength; j++) {
+                String token = tokens.get(j);
+                INDArray vector = wordVectors.getWordVectorMatrix(token);
+                features.put(new INDArrayIndex[]{NDArrayIndex.point(i),
+                        NDArrayIndex.all(),
+                        NDArrayIndex.point(j)},
+                    vector);
+
+                temp[1] = j;
+                featuresMask.putScalar(temp, 1.0);
+            }
+            int idx = category[i];
+            int lastIdx = Math.min(tokens.size(), maxLength);
+            labels.putScalar(new int[]{i, idx, lastIdx - 1}, 1.0);
+            labelsMask.putScalar(new int[]{i, lastIdx - 1}, 1.0);
+        }
+
+        DataSet ds = new DataSet(features, labels, featuresMask, labelsMask);
+        return ds;
     }
 
 }
